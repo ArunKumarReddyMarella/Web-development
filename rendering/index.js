90c59bc937efee40b17abfc41a64a5cb9c48d17c
Arun <arunkumarreddy159@gmail.com>
2021-11-21 17:19:27 +0530

    s

---
+++ rendering/index.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send("Arunreddy")
+})
+
+app.listen(port, () => {
+    console.log(`Listening on http://localhost:${port}`);
+})
