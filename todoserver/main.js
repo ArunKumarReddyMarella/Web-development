db116c33922a29b6f37284fc595bf4bac8ed9568
Arun <arunkumarreddy159@gmail.com>
2021-11-21 16:38:55 +0530

    fi

---
+++ todoserver/main.js
@@ -0,0 +1,54 @@
+const express = require('express')
+const fs = require('fs')
+const app = express()
+const port = 3000
+
+app.use(express.json())
+app.use(express.static("static"));
+
+app.use(function(req, res, next) {
+    console.log(req.url);
+    next();
+})
+
+app.post("/save", function(req, res) {
+    let data = JSON.stringify(req.body)
+    console.log("savejs  " + data);
+    fs.writeFile('./data.txt', data, (err) => {
+        res.end(data);
+    })
+})
+
+app.get("/getdata", function(req, res) {
+    readFile("./data.txt", function(err, data) {
+        res.end(data);
+    })
+})
+
+app.get("/gettodo", function(req, res) {
+    readFile("./data.txt", function(err, data) {
+        res.end(data);
+    })
+})
+
+app.get("/", (req, res) => {
+    readFile("./static/todoapphtml.html", function(err, data) {
+        if (err) {
+            res.end("error")
+            return
+        }
+        res.end(data)
+    })
+})
+
+
+function readFile(path, a) {
+    fs.readFile(path, function(err, data) {
+        a(err, data);
+    })
+}
+
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
